c70920f3601860f332f3414b8a95b4925ca26720
cijose <cijo@nabla.com>
2020-10-23 17:05:33 +0200

    Fix

--- src/power_iteration.py
+++ src/power_iteration.py
@@ -25,7 +25,7 @@ def power_iteration(fx, Mx, parameter, verbose=0, xtrue=None):
         info["fx"].append(fx(x))
         # Print the information.
         if verbose:
-            print("Iter = %4d, f(x) = %5.3e\n' %(iter,  info['fx'][iter])")
+            print('Iter = %4d, f(x) = %5.3e\n' %(iter,  info['fx'][iter]))
         # Start the clock.
         timestart = time.time()
         # Update the next iteration.
